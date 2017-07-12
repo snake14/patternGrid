68928992eda103a100d06703863c42046f79c1b3
jacobr <jacobr@Mint18>
2017-07-12 14:23:54 -0700

    Added missing semicolons for consistency.

--- store.js
+++ store.js
@@ -1,7 +1,7 @@
 const knex = require('knex')(require('./knexfile'))
 module.exports = {
   createGrid ({ name, grid, recent_colors, cell_size, grid_width, grid_height }) {
-    console.log(`Add grid with name ${name}, cell size ${cell_size}, width ${grid_width}, and height ${grid_height}`)
+    console.log(`Add grid with name ${name}, cell size ${cell_size}, width ${grid_width}, and height ${grid_height}`);
     return knex('pattern_grid').insert({
     	name,
     	grid, 
@@ -9,11 +9,11 @@ module.exports = {
     	cell_size, 
     	grid_width, 
     	grid_height
-    })
+    });
   },
   
   updateGrid ({ id, name, grid, recent_colors, cell_size, grid_width, grid_height }) {
-	  console.log(`Update grid ${id} with name ${name}, cell size ${cell_size}, width ${grid_width}, and height ${grid_height}`)
+	  console.log(`Update grid ${id} with name ${name}, cell size ${cell_size}, width ${grid_width}, and height ${grid_height}`);
 	  return knex('pattern_grid').where('id', id).update({
 		  id,
 		  name,
@@ -22,16 +22,16 @@ module.exports = {
 		  cell_size, 
 		  grid_width, 
 		  grid_height
-	   })
+	   });
   },
 	  
   deleteGrid (id) {
-	  console.log(`Delete grid ${id}`)
+	  console.log(`Delete grid ${id}`);
 	  return knex('pattern_grid').where('id', id).del();
   },
   
   getGrid (id) {
-	  console.log(`Get grid ${id}`)
+	  console.log(`Get grid ${id}`);
 	  return knex('pattern_grid').where('id', id).first().then(function(data) {
 		  return data;
 	  });
